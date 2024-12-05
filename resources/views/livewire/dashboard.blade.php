04806f40a256263ee551798e9947062f97b8d252
amarpaviall <er.amarpaviall@gmail.com>
2024-12-04 21:35:23 -0500

    Models and migrations

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -3,7 +3,17 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    //
+    public string $name = '';
+    public $startTime;
+
+    public function createListingParty(){
+
+    }
+    public function with() {
+        return [
+            'listening_parties' => ListeningParty::all(),
+        ];
+    }
 }; ?>
 
 <div>
